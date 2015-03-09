68c848a914f7412ae420bac0371f14ee83c372d8
Rocky Randalls <rrandalls@gmail.com>
2015-03-09 15:55:29 -0400

    Add indices and metrics

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 ### Run times
 **First seed run:** 23 min
 **First page load** 4.9 min
+**Page load after indexing:** 4.37 sec
 
 ### Indexing
 **Add index to sequences:** .0165 sec
-**Add index to genes:** .0812
-**Add index to hits:**
+**Add index to genes:** .0812 sec
+**Add index to hits:** .9635 sec
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309191715) do
+ActiveRecord::Schema.define(version: 20150309192135) do
 
   create_table "assemblies", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20150309191715) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
   end
+
+  add_index "hits", ["subject_id"], name: "index_hits_on_subject_id"
 
   create_table "sequences", force: :cascade do |t|
     t.text     "dna"
