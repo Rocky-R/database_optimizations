8d355afdfec569a1b25ec8df09e0064755da6fd0
Rocky Randalls <rrandalls@gmail.com>
2015-03-09 15:19:16 -0400

    Initial commit

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,13 +2,18 @@ class ReportsController < ApplicationController
   def all_data
     @assembly = Assembly.find_by_name(params[:name])
     @hits = []
-    @assembly.sequences.each do |s|
-      s.genes.each do |g|
-        g.hits.each do |h|
-          @hits << h
+
+    if @assembly.nil?
+      render nothing: true
+    else
+      @assembly.sequences.each do |s|
+        s.genes.each do |g|
+          g.hits.each do |h|
+            @hits << h
+          end
         end
+        @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
       end
     end
-    @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
   end
 end
